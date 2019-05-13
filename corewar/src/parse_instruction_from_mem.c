aa0128cecf247bf4d93915b11015cd379dd46592
Jamil ETTEL <jamil.ettel@epitech.com>
2019-05-13 14:15:29 +0200

    Fixed coding style errors

--- corewar/src/parse_instruction_from_mem.c
+++ corewar/src/parse_instruction_from_mem.c
@@ -8,23 +8,7 @@
 #include "my.h"
 #include "corewar.h"
 
-bool uses_indexes(int code)
-{
-    if (code == IC_ldi || code == IC_lldi || code == IC_fork ||
-        code == IC_lfork || code == IC_zjmp || code == IC_sti)
-        return (true);
-    return (false);
-}
-
-bool is_instruction_code_valid(char code)
-{
-    for (int i = 0; op_tab[i].code; i++)
-        if (op_tab[i].code == code)
-            return (true);
-    return (false);
-}
-
-int get_argument(proc_t *proc, int size)
+int get_arguments_from_mem(proc_t *proc, int size)
 {
     int argument = 0;
 
@@ -36,7 +20,7 @@ int get_argument(proc_t *proc, int size)
     return (argument);
 }
 
-bool get_description(proc_t *proc)
+bool get_description_from_mem(proc_t *proc)
 {
     proc->pc = proc->pc->next;
     if (has_one_argument(proc->instruction->code)) {
@@ -60,17 +44,10 @@ bool get_description(proc_t *proc)
     return (true);
 }
 
-bool parse_instruction_from_mem(proc_t *proc)
+void parse_arguments_from_mem(proc_t *proc)
 {
-    int size = 0;
+    int size;
 
-    proc->instruction->code = proc->pc->val;
-    if (!is_instruction_code_valid(proc->instruction->code)) {
-        proc->instruction->code = 0;
-        return (true);
-    }
-    if (!get_description(proc))
-        return (false);
     for (int i = 0; proc->instruction->description[i]; i++) {
         if (proc->instruction->description[i] == T_IND ||
             (proc->instruction->description[i] == T_DIR &&
@@ -80,7 +57,24 @@ bool parse_instruction_from_mem(proc_t *proc)
             size = DIR_SIZE;
         else
             size = 1;
-        proc->instruction->args[i] = get_argument(proc, size);
+        proc->instruction->args[i] = get_arguments_from_mem(proc, size);
     }
+}
+
+bool parse_instruction_from_mem(proc_t *proc)
+{
+    proc->instruction->code = proc->pc->val;
+    if (!is_instruction_code_valid(proc->instruction->code)) {
+        proc->instruction->code = 0;
+        return (true);
+    }
+    if (!get_description_from_mem(proc))
+        return (false);
+    if (!instruction_is_valid(proc->instruction)) {
+        proc->pc = proc->pc->prev;
+        proc->instruction->code = 0;
+        return (true);
+    }
+    parse_arguments_from_mem(proc);
     return (true);
 }
