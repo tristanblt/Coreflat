d0c9c87b0027747c2631a01d98b43098aa01f806
Jamil ETTEL <jamil.ettel@epitech.com>
2019-05-13 19:30:42 +0200

    re argument handling

--- corewar/include/corewar.h
+++ corewar/include/corewar.h
@@ -151,6 +151,8 @@ bool is_instruction_code_valid(char code);
 void dump_memory(list_t *mem);
 int get_nb_from_mem(list_t *mem, int size, int offset);
 proc_t **push_proc(proc_t **arr, proc_t *add);
+bool argument_error_handling(int ac, char **av);
+bool end_of_file(char *path, char *end);
 
 // list_t functions
 void add_next(list_t *list, int val);
---
+++ corewar/src/argument_error_handling.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2019
+** argument error handling
+** File description:
+** handles error for arguments
+*/
+
+#include "my.h"
+#include "corewar.h"
+
+bool argument_error_handling(int ac, char **av);
