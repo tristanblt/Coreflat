018ac671798bcf736856b9847f29c7de984aa78a
Jamil ETTEL <jamil.ettel@epitech.com>
2019-05-15 15:08:04 +0200

    fixed missing arg

--- corewar/bonus/src/main.c
+++ corewar/bonus/src/main.c
@@ -11,11 +11,12 @@ int main(int ac, char **av)
 {
     champion_t **champions = malloc(sizeof(champion_t *));
     list_t *memory = create_list();
+    int dump = -1;
 
     champions[0] = NULL;
     if (memory == NULL || !champions)
         return (EXIT_FAIL);
-    if (!argument_handling(ac, av, &champions))
+    if (!argument_handling(ac, av, &champions, &dump))
         return (EXIT_FAIL);
     if (!load_champion_instructions_in_mem(champions, memory))
         return (EXIT_FAIL);
