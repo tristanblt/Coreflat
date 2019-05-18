cf6fe46d38ab2c98fea58994619665aa38b9a12a
yoann le pech <yoann.le-pech@epitech.eu>
2019-05-18 23:31:46 +0200

    change champion in TU

--- corewar/tests/test_parse_instruction_from_mem.c
+++ corewar/tests/test_parse_instruction_from_mem.c
@@ -136,8 +136,8 @@ Test(corewar, simple_test)
     list_t *memory = create_list();
     int dump = -1;
     int ac = 3;
-    char *av[] = {"./corewar", "tests/files/test.cor",
-        "tests/files/test.cor", NULL};
+    char *av[] = {"./corewar", "tests/files/Gagnant.cor",
+        "tests/files/mortel.cor", NULL};
 
     champions[0] = NULL;
     argument_handling(ac, av, &champions, &dump);
@@ -153,6 +153,21 @@ Test(corewar, test_with_error)
     int ac = 13;
     char *av[] = {"./corewar", "-n", "10", "-a", "101", "tests/files/test.cor",
         "-a", "6000", "-n", "100", "tests/files/test.cor", "-dump", "12", NULL};
+
+    champions[0] = NULL;
+    argument_handling(ac, av, &champions, &dump);
+    load_champion_instructions_in_mem(champions, memory);
+    cr_assert(start_corewar(champions, memory, dump));
+}
+
+Test(corewar, simple_test_error)
+{
+    champion_t **champions = malloc(sizeof(champion_t *));
+    list_t *memory = create_list();
+    int dump = -1;
+    int ac = 3;
+    char *av[] = {"./corewar", "tests/files/ebola.cor",
+        "tests/files/maxidef.cor", NULL};
 
     champions[0] = NULL;
     argument_handling(ac, av, &champions, &dump);
